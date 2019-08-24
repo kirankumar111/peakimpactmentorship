902d996802577bdc9da4a3a01eb999fefce029be
Kiran Kumar <alienx@Kirans-MacBook-Pro-3.local>
2019-08-24 14:04:25 +0530

    Production update

--- oyl/controllers/kiravi.js
+++
@@ -1,6 +0,0 @@
-exports.getKiravi = (req, res, next) => {
-  res.render("kiravi", {
-    pageTitle: "Own Your Life",
-    isAuthenticated: req.session.isLoggedIn
-  });
-};
--- oyl/routes/kiravi.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const kiraviController = require("../controllers/kiravi");
-
-const router = express.Router();
-
-router.get("/kiravi", kiraviController.getKiravi);
-
-module.exports = router;
